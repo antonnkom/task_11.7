15daae401e01fe109ab33fa5d554567d76f28835
Anton Komarov <antonn.komm@gmail.com>
2024-02-18 22:28:23 +0300

    init commit

---
+++ functions.php
@@ -0,0 +1,24 @@
+<?php
+// входные данные Задания 1
+$a = [0, 0, 1, 1,];
+$b = [0, 1, 0, 1,];
+$i = 0;
+
+// входные данные Задания 2
+$arr = [true, false, 1, 0, -1, '1', null, 'php',];
+
+/**
+ * $item элемент входного массива
+ */
+function getNameCell($item) : string
+{
+    if (is_bool($item)) {
+        return $item ? 'true' : 'false';
+    } elseif (is_string($item)) {
+        return "\"$item\"";
+    } elseif (is_null($item)) {
+        return 'null';
+    } else {
+        return $item;
+    }
+}
---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="ru-RU">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Таблица истинности и таблица сравнений на PHP">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <title>Таблицы истинности и сравнения на PHP</title>
+</head>
+<body>
+    <main>
+        <?php include_once $_SERVER['DOCUMENT_ROOT'] . '/functions.php'; ?>
+
+        <section id="truth-table">
+            <div class="container">
+                <h2><span>Задание 1.</span> Таблица истинности PHP</h2>
+
+                <div class="table">
+                    <div class="trow">
+                        <div class="tcell">A</div>
+                        <div class="tcell">B</div>
+                        <div class="tcell">!A</div>
+                        <div class="tcell">A || B</div>
+                        <div class="tcell">A && B</div>
+                        <div class="tcell">A xor B</div>
+                    </div>
+
+                    <?php while ($i < count($a)): ?>
+                        <div class="trow">
+                            <div class="tcell"><?= $a[$i] ?></div>
+                            <div class="tcell"><?= $b[$i] ?></div>
+                            <div class="tcell"><?= ! $a[$i] ?: 0 ?></div>
+                            <div class="tcell"><?= ($a[$i] || $b[$i]) ? 1 : 0 ?></div>
+                            <div class="tcell"><?= ($a[$i] && $b[$i]) ? 1 : 0 ?></div>
+                            <div class="tcell"><?= ($a[$i] xor $b[$i]) ? 1 : 0 ?></div>
+                        </div>
+
+                        <?php $i++; ?>
+                    <?php endwhile; ?>
+                </div>
+            </div>  
+        </section>
+
+        <section id="flexible-comparison">
+            <div class="container">
+                <h2><span>Задание 2.</span> Гибкое сранение в PHP</h2>
+
+                <div class="table">
+                    <div class="trow">
+                        <div class="tcell"></div>
+
+                        <?php foreach ($arr as $a): ?>
+                            <div class="tcell"><?= getNameCell($a) ?></div>
+                        <?php endforeach; ?>
+                    </div>
+
+                    <?php foreach ($arr as $a): ?>
+                        <div class="trow">
+                            <div class="tcell"><?= getNameCell($a) ?></div>
+
+                            <?php foreach ($arr as $b): ?>
+                                <div class="tcell <?= $a != $b ?: 'true' ?>">
+                                    <?= $a == $b ? 'true' : 'false' ?>
+                                </div>
+                            <?php endforeach; ?>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+        </section>
+
+        <section id="tough-comparison">
+            <div class="container">
+                <h2><span>Задание 2.</span> Жёсткое сравнение в PHP</h2>
+
+                <div class="table">
+                    <div class="trow">
+                        <div class="tcell"></div>
+
+                        <?php foreach ($arr as $a): ?>
+                            <div class="tcell"><?= getNameCell($a) ?></div>
+                        <?php endforeach; ?>
+                    </div>
+                    
+                    <?php foreach ($arr as $a): ?>
+                        <div class="trow">
+                            <div class="tcell"><?= getNameCell($a) ?></div>
+
+                            <?php foreach ($arr as $b): ?>
+                                <div class="tcell <?= $a !== $b ?: 'true' ?>">
+                                    <?= $a === $b ? 'true' : 'false' ?>
+                                </div>
+                            <?php endforeach; ?>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+        </section>
+    </main>
+</body>
+</html>
